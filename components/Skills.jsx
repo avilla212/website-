063e214530045ee2b4f7007f25acec7e72f97d66
avilla212 <104694676+avilla212@users.noreply.github.com>
2023-04-28 20:56:32 -0700

    Still trying to fix bug

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import SkillsItem from '../components/SkillsItem'
 
+import C from '../public/assets/skills/c++.png'
+import git from '../public/assets/skills/github1.png'
+import html from '../public/assets/skills/html.png'
+import java from '../public/assets/skills/java.png'
+import next from '../public/assets/skills/nextjs.png'
+import python from '../public/assets/skills/python.png'
+import react from '../public/assets/skills/react.png'
+import tailwind from '../public/assets/skills/tailwind.png'
+
 const Skills = () => {
   return (
     <div id='skills' className=' w-full lg:h-screen p-2'>
@@ -8,34 +17,34 @@ const Skills = () => {
             <p className='text-xl tracking-widest uppercase py-8'>Skills</p>
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
 
-            <SkillsItem image='/../public/assets/skills/html.png'
+            <SkillsItem image={html}
             title='HTML'/>
             
-            <SkillsItem image='/../public/assets/skills/python.png'
+            <SkillsItem image={python}
             title='Python'
             />
 
-            <SkillsItem image='/../public/assets/skills/java.png' 
+            <SkillsItem image={java} 
             title='Java'
             />
 
-            <SkillsItem image='/../public/assets/skills/nextjs.png' 
+            <SkillsItem image={next}
             title='Next.js'
             />
 
-<SkillsItem image='/../public/assets/skills/react.png' 
+<SkillsItem image={react}
             title='React'
             />
 
-<SkillsItem image='/../public/assets/skills/tailwind.png' 
+<SkillsItem image={tailwind}
             title='Tailwind css'
             />
 
-<SkillsItem image='/../public/assets/skills/github1.png' 
+<SkillsItem image={git}
             title='Git'
             />
 
-<SkillsItem image='/../public/assets/skills/c++.png' 
+<SkillsItem image={C}
             title='C++'
             />
 

81b5994c65356874a807e92b79c90e3214d155d5
avilla212 <104694676+avilla212@users.noreply.github.com>
2023-04-28 20:49:20 -0700

    Trying to fix bug

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -8,12 +8,6 @@ const ProjectItem = ({title, backgroundImg, desc}) => {
                   <Image className=' group-hover:opacity-10' src={backgroundImg} alt='/'/>
                  
                     <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
-                      {/**
-                       * q: how do i move the h3 and p tags to the right of my image?
-                       * a:   1. use absolute positioning
-                       *  q: where do i use absolute positioning?
-                       * a:  1. use absolute positioning on the parent div
-                       */}
                       <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
                       <p>{desc}.</p>
                     </div>
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,101 +1,43 @@
 import React from 'react'
-import Image from 'next/image'
+import SkillsItem from '../components/SkillsItem'
 
 const Skills = () => {
   return (
     <div id='skills' className=' w-full lg:h-screen p-2'>
         <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
-            <p className='text-xl tracking-widest uppercase'>Skills</p>
-            <h2 className='py-4'>Skills I am progressing with </h2>
+            <p className='text-xl tracking-widest uppercase py-8'>Skills</p>
             <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
 
-              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-500'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                  <div className='m-auto'>
-                    <Image className='rounded-xl' src='/../public/assets/skills/html.png' width={100} height={100} alt='/' />
-                  </div>
-                  <div className='flex flex-col items-center justify-center'>
-                    <h3>HTML</h3>
-                  </div>
-                </div>
-              </div>
-              
-              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-500'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                  <div className='m-auto'>
-                    <Image className='rounded-xl' src='/../public/assets/skills/python.png' width={100} height={100} alt='/' />
-                  </div>
-                  <div className='flex flex-col items-center justify-center'>
-                    <h3>Python</h3>
-                  </div>
-                </div>
-              </div>
+            <SkillsItem image='/../public/assets/skills/html.png'
+            title='HTML'/>
+            
+            <SkillsItem image='/../public/assets/skills/python.png'
+            title='Python'
+            />
 
-              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-500'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                  <div className='m-auto'>
-                    <Image className='rounded-xl' src='/../public/assets/skills/java.png' width={100} height={100} alt='/' />
-                  </div>
-                  <div className='flex flex-col items-center justify-center'>
-                    <h3>Java</h3>
-                  </div>
-                </div>
-              </div>
+            <SkillsItem image='/../public/assets/skills/java.png' 
+            title='Java'
+            />
 
-              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-500'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                  <div className='m-auto'>
-                    <Image className='rounded-xl' src='/../public/assets/skills/nextjs.png' width={100} height={100} alt='/' />
-                  </div>
-                  <div className='flex flex-col items-center justify-center'>
-                    <h3>Next.js</h3>
-                  </div>
-                </div>
-              </div>
+            <SkillsItem image='/../public/assets/skills/nextjs.png' 
+            title='Next.js'
+            />
 
-              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-500'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                  <div className='m-auto'>
-                    <Image className='rounded-xl' src='/../public/assets/skills/react.png' width={100} height={100} alt='/' />
-                  </div>
-                  <div className='flex flex-col items-center justify-center'>
-                    <h3>React</h3>
-                  </div>
-                </div>
-              </div>
+<SkillsItem image='/../public/assets/skills/react.png' 
+            title='React'
+            />
 
-              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-500'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                  <div className='m-auto'>
-                    <Image className='rounded-xl' src='/../public/assets/skills/tailwind.png' width={100} height={100} alt='/' />
-                  </div>
-                  <div className='flex flex-col items-center justify-center'>
-                    <h3>Tailwind css</h3>
-                  </div>
-                </div>
-              </div>
+<SkillsItem image='/../public/assets/skills/tailwind.png' 
+            title='Tailwind css'
+            />
 
-              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-500'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                  <div className='m-auto'>
-                    <Image className='rounded-xl' src='/../public/assets/skills/github1.png' width={100} height={100} alt='/' />
-                  </div>
-                  <div className='flex flex-col items-center justify-center'>
-                    <h3>Github</h3>
-                  </div>
-                </div>
-              </div>
+<SkillsItem image='/../public/assets/skills/github1.png' 
+            title='Git'
+            />
 
-              <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-500'>
-                <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-                  <div className='m-auto'>
-                    <Image className='rounded-xl' src='/../public/assets/skills/c++.png' width={100} height={100} alt='/' />
-                  </div>
-                  <div className='flex flex-col items-center justify-center'>
-                    <h3>C++</h3>
-                  </div>
-                </div>
-              </div>
+<SkillsItem image='/../public/assets/skills/c++.png' 
+            title='C++'
+            />
 
             </div>
         </div>

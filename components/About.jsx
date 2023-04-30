6578a76582877e4030eb11b8ab438b4c00079157
avilla212 <104694676+avilla212@users.noreply.github.com>
2023-04-30 11:21:33 -0700

    changes to text

--- components/About.jsx
+++ components/About.jsx
@@ -14,15 +14,17 @@ const About = ({image}) => {
                   I am a Uni student back at community college studying Computer Science after pursuing Psychology. My background is in ABA Psychology as I have worked 
                   with people diagnosed with autism for 3 years. 
                   
-                  My passion for all things tech related began in my early teen years when I decided to take apart my Xbox 360 so I can mod video games which would lead to me building my own PC later on. 
-                  Since then I have alwyas had an interest in anything tech. In 2022, I decided to take the leap and pursue a career in tech. 
+                  My passion for all things tech related began in my early teen years when I decided to take apart my Xbox 360 to mod video games 
+                  which would then lead to me building my own PC later on. 
+                  
+                  Since then I've always had an interest in anything tech. In 2022, I decided to take my first steps towards pursuing a career in tech. 
                   I have been programming for 8 months on my own and have been learning from various online resources such as
                   <Link className='underline cursor-pointer' href='https://www.freecodecamp.org/'> FreeCodeCamp,</Link> 
                   <Link className='underline cursor-pointer' href='https://www.codecademy.com/'> Codecademy,</Link>
                   <Link className='underline cursor-pointer'  href='https://www.coursera.org/'> Coursera,</Link>
                   <Link className='underline cursor-pointer' href='https://www.theodinproject.com/'> The Odin Project </Link> and 
                   <Link className='underline cursor-pointer' href='https://leetcode.com/'> Leetcode.</Link>
-                   Programming aside, I enjoy working out, spending time with my family and catching up on the latest tech news.
+                   Programming aside, I enjoy working out, spending time with family, catching up with friends and catching up on the latest tech news.
                 </p>
             </div>
             <div className='w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 ease-in duration 300'>
